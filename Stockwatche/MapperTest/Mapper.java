d8c07a101aa2047b058cb0cc00ccf1ab6883973a
Sinan-Tayar <st071@hdm-stuttgart.de>
2019-12-26 21:39:35 +0100

    1 Committttt

--- Stockwatche/MapperTest/Mapper.java
+++ Stockwatche/MapperTest/Mapper.java
@@ -25,11 +25,258 @@ public static void main (String[]args) {
 		System.out.println(e);
 	}*/
 	
-	Group hdm = new Group();
-	GroupMapper gm = new GroupMapper();
+	//Group hdm = new Group();
+	//GroupMapper gm = new GroupMapper();
 	
-	hdm.setName("99errrr");
-	gm.insert(hdm);
+	//hdm.setName("99errrr");
+	//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
+	
+	/*try {
+	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Datenbank?user=root&password=trabzonspor61");
+	String query = "SELECT *  FROM kinogruppe";
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	while(rs.next()) {
+		System.out.println("email: "+ rs.getString("email"));
+	}
+	
+}catch(Exception e) {
+	System.out.println(e);
+}*/
+
+//Group hdm = new Group();
+//GroupMapper gm = new GroupMapper();
+
+//hdm.setName("99errrr");
+//gm.insert(hdm);
 	
 	
 }
